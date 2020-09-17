9b0b740b79a658ee0f201e3c30110190d0e7760c
tonytran021093 <59772082+tonytran021093@users.noreply.github.com>
2020-09-17 19:37:58 +0700

    1609
    1609

--- app/src/main/java/tht/app/random/Fragment1.java
+++ app/src/main/java/tht/app/random/Fragment1.java
@@ -4,6 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,6 +18,15 @@ import androidx.fragment.app.Fragment;
 
 public class Fragment1 extends Fragment {
 
+    ImageView imgHinhQuay;
+    ImageButton imbQuay;
+    TextView txtQuay;
+
+    int from = 0;
+
+    int[] so = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int[] vitri = {0, 36, 72, 108, 144, 180, 216, 252, 288, 324};
+    int[] vong = {1080, 1440, 1800};
 
     @Nullable
     @Override
@@ -22,6 +38,93 @@ public class Fragment1 extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        imgHinhQuay = view.findViewById(R.id.imgHinhQuay);
+        imbQuay = view.findViewById(R.id.imbQuay);
+        txtQuay = view.findViewById(R.id.txtQuay);
+
+        imbQuay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                xulyQuay();
+            }
+        });
+    }
+
+    private void xulyQuay() {
+        final int position = getRandom(vitri.length);
+        int positionVong = getRandom(vong.length);
+        int to = (int) vitri[position] + vong[positionVong];
+
+        imbQuay.setEnabled(false);
+        txtQuay.setAnimation(null);
+        txtQuay.setVisibility(View.INVISIBLE);
+
+        final RotateAnimation animation1 = new RotateAnimation(from, to, Animation.RELATIVE_TO_SELF, 0.5F,
+                Animation.RELATIVE_TO_SELF, 0.5F);
+        animation1.setDuration(5000);
+        animation1.setInterpolator(new DecelerateInterpolator());
+        animation1.setFillAfter(true);
+
+        animation1.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                switch (so[position]) {
+                    case 1:
+                        txtQuay.setText("1");
+                        break;
+                    case 2:
+                        txtQuay.setText("2");
+                        break;
+                    case 3:
+                        txtQuay.setText("3");
+                        break;
+                    case 4:
+                        txtQuay.setText("4");
+                        break;
+                    case 5:
+                        txtQuay.setText("5");
+                        break;
+                    case 6:
+                        txtQuay.setText("6");
+                        break;
+                    case 7:
+                        txtQuay.setText("7");
+                        break;
+                    case 8:
+                        txtQuay.setText("8");
+                        break;
+                    case 9:
+                        txtQuay.setText("9");
+                        break;
+                    default:
+                        txtQuay.setText("10");
+                        break;
+                }
+                Animation animascale = AnimationUtils.loadAnimation(getContext(), R.anim.anim_scale);
+                txtQuay.startAnimation(animascale);
+
+                imbQuay.setEnabled(true);
+                txtQuay.setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        imgHinhQuay.startAnimation(animation1);
+        from = (int) vitri[position];
+
+    }
+
+    public int getRandom(int max) {
+        return (int) (Math.random() * max);
     }
 }
 
--- app/src/main/java/tht/app/random/Fragment2.java
+++ app/src/main/java/tht/app/random/Fragment2.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,15 +14,40 @@ import androidx.fragment.app.Fragment;
 
 public class Fragment2 extends Fragment {
 
+    TextView txtKQ2;
+    EditText edtMin,edtMax;
+    ImageButton imbRanDom2;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         return inflater.inflate(R.layout.fragment2_layout, container, false);
     }
-
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        txtKQ2 = view.findViewById(R.id.txtKQ2);
+        edtMin = view.findViewById(R.id.edtMin);
+        edtMax = view.findViewById(R.id.edtMax);
+        imbRanDom2 = view.findViewById(R.id.imbRanDom2);
+
+        imbRanDom2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                xulyRandom();
+            }
+        });
+    }
+
+    private void xulyRandom() {
+        int so1 = Integer.parseInt(edtMin.getText().toString());
+        int so2 = Integer.parseInt(edtMax.getText().toString());
+        
+    }
+
+    public int getRandomMinMax(int min,int max) {
+        return (int) Math.floor(Math.random() * (max - min)) + min;
     }
 }
